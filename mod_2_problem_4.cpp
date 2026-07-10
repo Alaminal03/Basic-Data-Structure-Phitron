ddcc46290f65e8f67d71b0228f19f09f4f4b8213
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-07-11 01:54:29 +0600

    Add files via upload

---
+++ mod_2_problem_4.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,count=0;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n;i++)
+    {
+        cin >> v[i];
+    }
+
+    for(int i = 0; i < n;i++)
+    {
+        for(int j = 0; j < n;j++)
+        {
+            if(v[j]==v[i]+1)
+            {
+                count++;
+                break;
+            }
+        }
+    } 
+    cout << count;
+
+    return 0;
+}
