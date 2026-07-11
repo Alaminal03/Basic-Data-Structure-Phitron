2ba03d58fa3847f4fa950c3cbf9e5f0f6b5d88f4
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-07-12 00:46:53 +0600

    Add files via upload

---
+++ mod_2_problem_5.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<string> v;
+    string s;
+    while(cin >> s)
+    {
+        v.push_back(s);
+    }
+        cout << s << endl;
+
+
+    return 0;
+}
