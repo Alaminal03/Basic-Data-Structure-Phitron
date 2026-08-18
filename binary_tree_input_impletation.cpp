037588ca03761edaf863c87263103eeb73d3a552
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-08-19 00:02:37 +0600

    Add files via upload

---
+++ binary_tree_input_impletation.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node *left;
+        Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }    
+};
+
+Node* input_tree()
+{
+   int val;
+    cin >> val;
+    Node *root = new Node(val);
+    queue<Node *> q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *myleft, *myright;
+        if(l==-1)
+            myleft = NULL;
+        else
+            myleft = new Node(l);
+
+        if(r==-1)
+            myright = NULL;
+        else
+            myright = new Node(r);
+
+        f->left = myleft;
+        f->right = myright;
+
+        if(f->left!=NULL)
+            q.push(f->left);
+        if(f->right!=NULL)
+            q.push(f->right);
+     }
+     return root;
+ }  
+
+void level_order(Node* root)
+{
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        Node *f = q.front();        //Node ber kore ana
+        q.pop();
+
+        cout << f->val <<" ";       //oi Node ke niye kaj kore
+
+        if(f->left !=NULL)          //children push kora
+            q.push(f->left);
+        if(f->right != NULL)
+            q.push(f->right);
+    }
+} 
+
+
+int main()
+{
+    Node *root = input_tree();
+    level_order(root);
+
+    return 0;
+}
